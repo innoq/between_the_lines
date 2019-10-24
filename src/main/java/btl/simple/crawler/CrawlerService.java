33536e5831beb877545cbcaf43284655e0a3d75d
felix <felix.schumacher@innoq.com>
2019-10-24 14:46:24 +0200

    Example for JSoup

---
+++ src/main/java/btl/simple/crawler/CrawlerService.java
@@ -0,0 +1,20 @@
+package btl.simple.crawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class CrawlerService {
+
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        System.out.println(doc.title());
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            System.out.println("title: "+ headline.attr("title")+" url: "+ headline.absUrl("href"));
+        }
+    }
+}
