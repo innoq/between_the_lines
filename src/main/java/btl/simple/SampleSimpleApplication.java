968805fd924bf93175214c5064e6dfcb7bbd112b
arnel <arne.landwehr@innoq.com>
2019-10-24 13:56:16 +0200

    remove unused imports

--- src/main/java/btl/simple/SampleSimpleApplication.java
+++ src/main/java/btl/simple/SampleSimpleApplication.java
@@ -16,20 +16,15 @@
 
 package btl.simple;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import btl.simple.service.HelloWorldService;
 
 @EnableAutoConfiguration
 @ComponentScan
 public class SampleSimpleApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleSimpleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SampleSimpleApplication.class, args);
+  }
 }
